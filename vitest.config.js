b322758bb8e24238b414f63b23f2c50d835755e3
Jack <jacklukic@gmail.com>
2024-02-12 18:26:38 -0500

    Config for vitest

--- vitest.config.js
+++ vitest.config.js
@@ -1,5 +1,18 @@
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+  server: {
+    host: true,
+  },
+  test: {
+    coverage: {
+      enabled: true,
+      provider: 'istanbul',
+      reporter: ['text', 'json-summary', 'json'],
+      include: [
+        'packages/**/src/**/*.js'
+      ]
+    }
+  },
   workspace: './vitest.workspace.js'
 });
