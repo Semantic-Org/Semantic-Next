745ebaf96bf38f2aa9a706dbb661d6284e3739a5
Jack <jacklukic@gmail.com>
2024-02-12 18:22:06 -0500

    Add vite config

--- vitest.config.js
+++ vitest.config.js
@@ -1,18 +1,5 @@
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  server: {
-    host: true,
-  },
-  test: {
-    coverage: {
-      enabled: true,
-      provider: 'istanbul',
-      reporter: ['json', 'html'],
-      include: [
-        'packages/**/src/**/*.js'
-      ]
-    }
-  },
   workspace: './vitest.workspace.js'
 });
