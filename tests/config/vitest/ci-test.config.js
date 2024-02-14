45693719679560192ae1d5a5e1560994349b6cc6
jlukic <jacklukic@gmail.com>
2024-02-14 16:04:53 -0500

    Remove unused dir

--- tests/config/vitest/ci-test.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vitest/config';
-
-export default defineConfig({
-  test: {
-    reporter: ['junit'],
-    outputFile: './tests/results/test-results-junit.xml'
-  },
-  workspace: './vitest.workspace.js'
-});
