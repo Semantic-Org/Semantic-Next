d559d1796f4e4764d2092fea2c0390276cb6e535
jlukic <jacklukic@gmail.com>
2024-02-20 14:33:15 -0500

    Add self signed cert, docs setup

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,13 +1,25 @@
 import { defineConfig } from 'astro/config';
+import fs from 'fs';
 
 // https://astro.build/config
 export default defineConfig({
 
-  site: 'https://next.semantic-ui.com',
+  //site: 'https://next.semantic-ui.com',
 
   server: {
     host: true,
-    port: 8000,
+    port: 443,
   },
+
+  vite: {
+    server: {
+      // SSL for localhost and dev.semantic-ui.com
+      // add '127.0.0.1 dev.semantic-ui.com ' to your 'etc/hosts' file to use
+      https: {
+        key: fs.readFileSync('./cert/dev-key.pem'),
+        cert: fs.readFileSync('./cert/dev.pem')
+      },
+    }
+  }
 
 });
