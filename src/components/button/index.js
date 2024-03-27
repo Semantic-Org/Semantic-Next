93ef577fc3c11c11ba4b68c28701298ae1f8f7e8
jlukic <jacklukic@gmail.com>
2024-03-27 19:27:55 -0400

    Change name

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,3 +1,3 @@
 export { UIButtons } from './plural/buttons.js';
-export { ButtonOr } from './subcomponents/or/button-or.js';
+export { ButtonOr } from './content/or/button-or.js';
 export { UIButton } from './button.js';
