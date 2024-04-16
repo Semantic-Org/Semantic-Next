de76620fba21d62d03c19ac738663fd93753065b
Jack <jacklukic@gmail.com>
2024-04-16 17:28:07 -0400

    Try out expressive

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,17 +1,19 @@
 import { defineConfig } from 'astro/config';
 import fs from 'fs';
-
 import lit from '@astrojs/lit';
 import mdx from '@astrojs/mdx';
+
+import expressiveCode from "astro-expressive-code";
 
 // https://astro.build/config
 export default defineConfig({
   //site: 'https://next.semantic-ui.com',
-  devToolbar: { enabled: false },
-
+  devToolbar: {
+    enabled: false
+  },
   server: {
     host: true,
-    port: 443,
+    port: 443
   },
   vite: {
     assetsInclude: ['**/*.html'],
@@ -20,9 +22,9 @@ export default defineConfig({
       // add '127.0.0.1 dev.semantic-ui.com ' to your 'etc/hosts' file to use
       https: {
         key: fs.readFileSync('./cert/dev-key.pem'),
-        cert: fs.readFileSync('./cert/dev.pem'),
-      },
-    },
+        cert: fs.readFileSync('./cert/dev.pem')
+      }
+    }
   },
-  integrations: [lit(), mdx()],
+  integrations: [lit(), expressiveCode(), mdx()]
 });
