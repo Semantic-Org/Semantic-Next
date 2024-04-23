f6df5beb53b57749edd5c5ce51efc416302cd71f
jlukic <jacklukic@gmail.com>
2024-04-22 23:12:20 -0400

    Fix some when not array, add asyncEach and map

--- packages/utils/src/utils.js
+++ packages/utils/src/utils.js
@@ -359,7 +359,10 @@ export const flatten = (arr) => {
 };
 
 export const some = (collection, predicate) => {
-  return collection.some(predicate);
+  return (collection?.some)
+    ? collection.some(predicate)
+    : false
+  ;
 };
 export const any = some;
 
@@ -592,6 +595,62 @@ export const each = (obj, func, context) => {
   return obj;
 };
 
+export const asyncEach = async (obj, func, context) => {
+  if (obj === null || obj === undefined) {
+    return obj;
+  }
+  const iteratee = context ? func.bind(context) : func;
+  if (isObject(obj) || isFunction(obj)) {
+    if (obj.length !== undefined && typeof obj.length === 'number') {
+      obj = Array.from(obj);
+    }
+  }
+  if (isArray(obj)) {
+    for (let i = 0; i < obj.length; ++i) {
+      if (await iteratee(obj[i], i, obj) === false) {
+        break;
+      }
+    }
+  }
+  else {
+    const keys = Object.keys(obj);
+    for (let key of keys) {
+      if (await iteratee(obj[key], key, obj) === false) {
+        break;
+      }
+    }
+  }
+  return obj;
+};
+
+/*
+  Asynchronous mapping over objects and arrays
+*/
+export const asyncMap = async (obj, func, context) => {
+  if (obj === null || obj === undefined) {
+    return obj;
+  }
+  const iteratee = context ? func.bind(context) : func;
+  const result = isArray(obj) ? [] : {};
+  if (isObject(obj) || isFunction(obj)) {
+    if (obj.length !== undefined && typeof obj.length === 'number') {
+      obj = Array.from(obj);
+    }
+  }
+  if (isArray(obj)) {
+    for (let i = 0; i < obj.length; ++i) {
+      result[i] = await iteratee(obj[i], i, obj);
+    }
+  }
+  else {
+    const keys = Object.keys(obj);
+    for (let key of keys) {
+      result[key] = await iteratee(obj[key], key, obj);
+    }
+  }
+  return result;
+};
+
 /*-------------------
        RegExp
 --------------------*/
@@ -795,6 +854,7 @@ export const isEqual = (a, b, options = {}) => {
   // true if both NaN, false otherwise
   return a !== a && b !== b;
 };
+
 
 /*-------------------
       Constants
