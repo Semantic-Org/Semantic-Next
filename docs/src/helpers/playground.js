5be6c358e0bfc517127e605843ef6daf68f188be
Jack <jacklukic@gmail.com>
2024-04-24 15:48:55 -0400

    Toying

---
+++ docs/src/helpers/playground.js
@@ -0,0 +1,15 @@
+export const htmlBefore = `<!-- playground-hide -->
+<html>
+<head>
+<link href="./index.css" rel="stylesheet">
+<script src="./index.js" type="module"></script>
+<link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+</head>
+<body>
+<!-- playground-hide-end -->`;
+
+export const htmlAfter = `<!-- playground-hide --></body></html><!-- playground-hide-end -->`;
+
+export const cssBefore = `/* playground-hide */ body { padding 1rem; font-family: Lato; } /* playground-hide-end */`;
+
+export const jsBefore = `/* playground-hide */import '@semantic-ui/core';/* playground-hide-end */`;
