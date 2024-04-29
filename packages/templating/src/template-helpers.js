00049007ce2f3f6edb675bb2d43dd1adbe44b2d6
Jack <jacklukic@gmail.com>
2024-04-29 17:35:48 -0400

    Did i fix dep hell?

--- packages/templating/src/template-helpers.js
+++ packages/templating/src/template-helpers.js
@@ -1,5 +1,5 @@
 import { Reaction } from '@semantic-ui/reactivity';
-import { capitalize, toTitleCase, isEmpty, formatDate, isEmpty, tokenize } from '@semantic-ui/utils';
+import { capitalize, toTitleCase, isEmpty, formatDate, tokenize } from '@semantic-ui/utils';
 
 /*
   These could be written in shorthand but its easier for debugging
