c41c4d6dc8c9c505aefb03a6b31d5e507ca7e68e
jlukic <jacklukic@gmail.com>
2024-04-30 11:08:00 -0400

    Work on playground

--- docs/src/components/CodePlayground/CodePlayground.js
+++ docs/src/components/CodePlayground/CodePlayground.js
@@ -1,5 +1,5 @@
 import { createComponent } from '@semantic-ui/component';
-import { get } from '@semantic-ui/utils';
+import { get, each } from '@semantic-ui/utils';
 import { ReactiveVar } from '@semantic-ui/reactivity';
 
 import template from './CodePlayground.html?raw';
@@ -15,15 +15,63 @@ const settings = {
 };
 
 const createInstance = ({tpl, settings, $}) => ({
-  getFileType(type) {
-    const types = {
-      'text/css': 'sample/css',
-      'text/html': 'sample/html',
-      'text/javascript': 'sample/js',
-      'text/typescript': 'sample/ts',
+  scriptTypes: {
+    'text/css': 'sample/css',
+    'text/html': 'sample/html',
+    'text/javascript': 'sample/js',
+    'text/typescript': 'sample/ts',
+  },
+  panelIndex: {
+    'component.js': 0,
+    'component.html': 0,
+    'component.css': 0,
+    'index.html': 1,
+    'index.css': 1,
+    'index.js': 1,
+  },
+  positionIndex: {
+    'component.js': 0,
+    'component.html': 1,
+    'component.css': 2,
+    'index.html': 0,
+    'index.css': 1,
+    'index.js': 2,
+  },
+  getScriptType(type) {
+    return get(tpl.scriptTypes, type);
+  },
+  getFileArray() {
+    let files = [];
+    each(settings.files, (file, filename) => {
+      const fileData = tpl.getFile(file, filename);
+      console.log(fileData);
+      files.push(fileData);
+    });
+    console.log(files);
+    return files;
+  },
+  getFile(file, filename) {
+    return {
+      _id: filename,
+      filename,
+      ...file,
+      scriptType: tpl.getScriptType(file.contentType),
+      panelIndex: tpl.getPanelIndex(filename),
+      positionIndex: tpl.getPositionIndex(filename),
     };
-    return get(types, type);
-  }
+  },
+  getPanelIndex(filename) {
+    console.log(tpl.panelIndex, filename, get(tpl.panelIndex, filename));
+    return get(tpl.panelIndex, filename);
+  },
+  getPositionIndex(filename) {
+    return get(tpl.positionIndex, filename);
+  },
+  getPanes() {
+    let files = tpl.getFileArray();
+    let panes = [];
+    return panes;
+  },
 });
 
 const onCreated = ({ tpl }) => {
