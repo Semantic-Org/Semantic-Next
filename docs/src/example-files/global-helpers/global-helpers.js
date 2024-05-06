c7125034aacf0790fcccf21bd15dab2b4d1911de
jlukic <jacklukic@gmail.com>
2024-05-06 15:30:08 -0400

    Typo

--- docs/src/example-files/global-helpers/global-helpers.js
+++ docs/src/example-files/global-helpers/global-helpers.js
@@ -1,7 +1,6 @@
 import { createComponent, getText } from '@semantic-ui/component';
 import { ReactiveVar } from '@semantic-ui/reactivity';
 
-const css = await getText('./component.css');
 const template = await getText('./component.html');
 
 const createInstance = ({tpl}) => ({
