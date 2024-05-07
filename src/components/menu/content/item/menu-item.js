ba61c65ffb5f98dbe7113e51285a32519452a9ca
Jack <jacklukic@gmail.com>
2024-05-07 18:13:38 -0400

    Start on menu theme

--- src/components/menu/content/item/menu-item.js
+++ src/components/menu/content/item/menu-item.js
@@ -3,8 +3,13 @@ import { createComponent } from '@semantic-ui/component';
 import ItemCSS from './menu-item.css?raw';
 import ItemTemplate from './menu-item.html?raw';
 
+const settings = {
+  active: false,
+};
+
 export const MenuItem = createComponent({
   tagName: 'menu-item',
+  settings,
   template: ItemTemplate,
   css: ItemCSS,
 });
