dfabf50755846ebba86e33b3b69066be6e74ce1b
jlukic <jacklukic@gmail.com>
2024-05-08 14:53:26 -0400

    Go to town on menu

--- src/components/menu/content/item/menu-item.js
+++ src/components/menu/content/item/menu-item.js
@@ -4,8 +4,18 @@ import { MenuItemComponentSpec } from '@semantic-ui/specs';
 import MenuItemCSS from './menu-item.css?raw';
 import MenuItemTemplate from './menu-item.html?raw';
 
+const events = {
+  'touchstart .menu'({event, tpl, $}) {
+    $(this).addClass('pressed');
+  },
+  'touchend .menu'({event, tpl, $}) {
+    $(this).removeClass('pressed');
+  },
+};
+
 export const MenuItem = createComponent({
   tagName: 'menu-item',
+  events: events,
   componentSpec: MenuItemComponentSpec,
   template: MenuItemTemplate,
   css: MenuItemCSS,
