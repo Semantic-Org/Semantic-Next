5e1a5d445378dd26fe8af804e1c926e0428abf6f
jlukic <jacklukic@gmail.com>
2024-06-12 13:14:02 -0400

    Work on mobile fixes

--- docs/src/examples/component/minimal/component.js
+++ docs/src/examples/component/minimal/component.js
@@ -2,11 +2,10 @@ import { createComponent } from '@semantic-ui/component';
 
 createComponent({
   tagName: 'ui-counter',
-  template: '<div>{{counter}}</div>',
-  css: 'div { background-color: red; padding: 1rem; }',
+  template: '<div>Counter is <b>{{counter}}</b></div>',
+  css: 'div { border: var(--border); color: var(--standard-80); padding: var(--spacing); }',
   state: { counter: 0 },
   onCreated({state}) {
     setInterval(() => state.counter.increment(), 1000);
   }
 });
-/* playground-fold-end */
