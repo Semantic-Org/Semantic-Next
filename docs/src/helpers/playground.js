273b9fd15d9810b44b5c4d31cb0777e3725ccdec
Jack <jacklukic@gmail.com>
2024-12-09 18:22:47 -0500

    Broke

--- docs/src/helpers/playground.js
+++ docs/src/helpers/playground.js
@@ -45,7 +45,7 @@ export const getExampleFiles = async({
   }
   let hasComponent = false;
   let exampleFiles = {};
-  const pathRegExp = new RegExp(`${basePath}.*/${contentID}/${subFolder}`);
+  const pathRegExp = new RegExp(`${basePath}.*${contentID}/${subFolder}`);
   await asyncEach(allFiles, async (file, path) => {
     if (path.match(pathRegExp)) {
       const fileName = path.replace(pathRegExp, '').replace('/', '');
