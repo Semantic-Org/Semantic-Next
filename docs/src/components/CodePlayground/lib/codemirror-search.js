10a984ce19a971bcc8af65dbcb2feb175d8bf07b
Jack <jacklukic@gmail.com>
2025-02-03 18:17:05 -0500

    Fix bug in resize of codeplayground

--- docs/src/components/CodePlayground/lib/codemirror-search.js
+++ docs/src/components/CodePlayground/lib/codemirror-search.js
@@ -7,7 +7,7 @@ export const addSearch = function(CodeMirror) {
   var Pos = CodeMirror.Pos
 
   function regexpFlags(regexp) {
-    var flags = regexp.flags
+    var flags = regexp?.flags;
     return flags != null ? flags : (regexp.ignoreCase ? "i" : "")
       + (regexp.global ? "g" : "")
       + (regexp.multiline ? "m" : "")
