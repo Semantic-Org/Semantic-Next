8501e393befa92d9f081f2858cd505dfadf6b94e
Jack Lukic <jacklukic@gmail.com>
2025-02-19 23:01:26 -0500

    Attempting to get playground autocomplete working

--- packages/reactivity/types/signal.d.ts
+++ packages/reactivity/types/signal.d.ts
@@ -8,20 +8,21 @@ export interface SignalOptions<T> {
    * @param newValue - The new value to compare against
    */
    equalityFunction?: (oldValue: T, newValue: T) => boolean;
-  
+
    /**
    * Whether to allow cloning of values. If false, values are stored by reference
    * @default true
    */
    allowClone?: boolean;
-  
+
    /**
    * Custom function to clone values when storing or retrieving from the signal
    * @param value - The value to clone
    */
    cloneFunction?: <V>(value: V) => V;
-  }
-  
+}
+
+
 /**
 * A Signal represents a reactive value that automatically triggers updates
 * when modified. It can store any type of value and provides methods for
