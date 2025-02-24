1473692d165b104abe036af389835c71b7504f97
jlukic <jacklukic@gmail.com>
2025-02-24 10:21:46 -0500

    HMR fix, dead link fixes

--- packages/component/types/index.d.ts
+++ packages/component/types/index.d.ts
@@ -1,6 +1,9 @@
+export { getText, getJSON } from '@semantic-ui/utils';
+
 export { DefineComponentOptions, defineComponent, UIWebComponent } from './define-component';
 export { WebComponentBase } from './web-component';
 
 export { adoptStylesheet } from './helpers/adopt-stylesheet';
 export { scopeStyles } from './helpers/scope-styles';
 export { extractCSS } from './helpers/extract-css';
+
