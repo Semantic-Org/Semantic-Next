349acc690bb55f313c0581b850b2c28d86bc0554
Jack <jacklukic@gmail.com>
2025-03-03 14:23:04 -0500

    Fix autocomplete broke in docs

--- docs/src/types/index.d.ts
+++ docs/src/types/index.d.ts
@@ -1,9 +1,6 @@
 declare module '*.js';
 declare module '@components/*';
 declare module '@helpers/*';
-declare module '@semantic-ui/core';
-declare module '@semantic-ui/utils';
-
 declare module '@components/*.js' {
   const component: any;
   export default component;
