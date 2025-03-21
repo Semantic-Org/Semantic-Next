a364588e452907e450f5829db809fb002153ad9e
Jack Lukic <jacklukic@gmail.com>
2025-03-21 18:03:35 -0400

    Bug: add missing cyc check condition for simple expression matching.

--- packages/renderer/src/lit/renderer.js
+++ packages/renderer/src/lit/renderer.js
@@ -377,6 +377,7 @@ export class LitRenderer {
     if (simpleExpression) {
       const value = this.lookupTokenValue(expression, data);
       if (value !== undefined) {
+        visited.delete(expression);
         return wrapFunction(value)();
       }
     }
