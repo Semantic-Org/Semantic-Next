c20e663650db8bd4507274967c58ae9e4cd5dbef
Jack Lukic <jacklukic@gmail.com>
2025-04-02 15:37:29 -0400

    Docs: Fix service worker to work for jsdelivr

--- docs/public/cdn-redirector.js
+++ docs/public/cdn-redirector.js
@@ -10,7 +10,8 @@ self.addEventListener('fetch', (event) => {
   // Check if this request is for a CDN we want to redirect
   if (newHost) {
     // Create a new URL using jsdelivr instead of unpkg
-    const newUrl = requestURL.replace(url.hostname, newHost) + '+esm';
+    let newUrl = requestURL.replace(url.hostname, newHost);
+    newUrl = requestURL.replace('package.json', '+esm');
     console.log(`CDN redirector: ${url} → ${newUrl}`);
     
     event.respondWith(
