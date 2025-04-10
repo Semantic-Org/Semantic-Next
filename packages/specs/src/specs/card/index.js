5fd79d15f67bf5ad98706e6d6c34842b8b8d7f88
Jack <jacklukic@gmail.com>
2025-04-10 15:13:16 -0400

    Chore: Remove ui specs from spec package

--- packages/specs/src/specs/card/index.js
+++
@@ -1,9 +0,0 @@
-import { SpecReader } from '../../spec-reader.js';
-
-import CardSpec from './card.json' with { type: 'json' };
-
-const CardComponentSpec = new SpecReader(CardSpec).getWebComponentSpec();
-const CardPluralComponentSpec = new SpecReader(CardSpec, { plural: true }).getWebComponentSpec();
-
-export default CardSpec;
-export { CardComponentSpec, CardPluralComponentSpec, CardSpec };
--- packages/specs/src/specs/icon/index.js
+++
@@ -1,8 +0,0 @@
-import { SpecReader } from '../../spec-reader.js';
-import IconSpec from './icon.json' with { type: 'json' };
-const reader = new SpecReader();
-const IconComponentSpec = reader.getWebComponentSpec(IconSpec);
-
-export default IconSpec;
-
-export { IconComponentSpec, IconSpec };
