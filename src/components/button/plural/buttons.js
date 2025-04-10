037a380717af2241d09390ab67696f6cb297c4e7
Jack <jacklukic@gmail.com>
2025-04-10 14:49:37 -0400

    chore: Add in component build for buttons

--- src/components/button/plural/buttons.js
+++ src/components/button/plural/buttons.js
@@ -1,18 +1,18 @@
 import { defineComponent } from '@semantic-ui/component';
-import { ButtonPluralComponentSpec } from '@semantic-ui/specs';
+import componentSpec from '../spec/buttons-component.json';
 
-import ButtonPageCSS from '../button-page.css?raw';
-import ButtonCSS from '../button-bundle.css?raw';
+import pageCSS from '../button-page.css?raw';
+import css from '../button-bundle.css?raw';
 
-import ButtonsTemplate from './buttons.html?raw';
+import template from './buttons.html?raw';
 
 export const UIButtons = defineComponent({
   tagName: 'ui-buttons',
   plural: true,
   singularTag: 'ui-button',
   delegateFocus: true,
-  componentSpec: ButtonPluralComponentSpec,
-  template: ButtonsTemplate,
-  css: ButtonCSS,
-  pageCSS: ButtonPageCSS,
+  componentSpec,
+  template,
+  css,
+  pageCSS,
 });
