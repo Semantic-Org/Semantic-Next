437596420bd28e2c3e6be15d0e99159d48581770
jlukic <jacklukic@gmail.com>
2025-06-11 20:16:02 -0400

    Docs: modify tailwind example to use settings, page css, and SUI next to tailwinds

--- docs/src/examples/tailwind/component.js
+++ docs/src/examples/tailwind/component.js
@@ -5,12 +5,7 @@ const template = await getText('./component.html');
 const css = await getText('./component.css');
 
 const defaultSettings = {
-  proofPoints: [
-    `Customizing your theme with <code class="font-mono font-medium text-gray-950 dark:text-white">@theme</code>`,
-    `Adding custom utilities with <code class="font-mono font-medium text-gray-950 dark:text-white">@utility</code>`,
-    `Adding custom variants with <code class="font-mono font-medium text-gray-950 dark:text-white">@variant</code>`,
-    `Code completion with instant preview`,
-  ],
+  proofPoints: [],
 };
 
 let definition = {
---
+++ docs/src/examples/tailwind/page.js
@@ -0,0 +1,10 @@
+import { $ } from '@semantic-ui/query';
+
+$('tailwind-demo').settings({
+  proofPoints: [
+    `Customizing your theme with <code>@theme</code>`,
+    `Adding custom utilities with <code>@utility</code>`,
+    `Adding custom variants with <code>@variant</code>`,
+    `Code completion with instant preview`,
+  ],
+});
