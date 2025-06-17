b072d42481e4bacee832bb0c7d50814ab61cfeac
jlukic <jacklukic@gmail.com>
2025-06-16 21:09:55 -0400

    Hardcode tailwind bundle for now

--- docs/src/pages/examples/importmap.json.js
+++ docs/src/pages/examples/importmap.json.js
@@ -85,6 +85,9 @@ for (const pkg of importPackages) {
   }
 }
 
+// hardcode this for now
+packageImports['@semantic-ui/tailwind'] = 'https://cdn.jsdelivr.net/npm/@semantic-ui/tailwind/dist/bundle/tailwind.js';
+
 export const importMap = packageImports;
 export const importMapJSON = JSON.stringify(packageImports, null, 2);
 
