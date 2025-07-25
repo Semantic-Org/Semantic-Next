b512b0508908df9f9e2853143184af5a932f441d
Jack Lukic <jacklukic@gmail.com>
2025-07-25 15:22:56 -0400

    Fix some broken pages

--- docs/src/examples/templates/expressions-boolean-attributes/component.js
+++ docs/src/examples/templates/expressions-boolean-attributes/component.js
@@ -3,14 +3,27 @@ import { defineComponent, getText } from '@semantic-ui/component';
 const css = await getText('./component.css');
 const template = await getText('./component.html');
 
-const defaultState = {};
+const defaultState = {
+  checked: false
+};
 
-const createComponent = ({ state }) => ({});
+const createComponent = ({ state }) => ({
+  toggleCheckbox() {
+    state.checked.toggle();
+  }
+});
+
+const events = {
+  'click ui-button'({ self }) {
+    self.toggleCheckbox();
+  }
+};
 
 defineComponent({
   tagName: 'boolean-attributes',
   template,
   css,
+  events,
   defaultState,
   createComponent,
 });
