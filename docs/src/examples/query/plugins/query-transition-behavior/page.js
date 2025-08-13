1756e99eaf1e9a0e94a307e7464ad36eddfe2f99
Jack Lukic <jacklukic@gmail.com>
2025-08-13 17:12:47 -0400

    Example: Update transition example

--- docs/src/examples/query/plugins/query-transition-behavior/page.js
+++ docs/src/examples/query/plugins/query-transition-behavior/page.js
@@ -2,5 +2,5 @@ import { Transition } from '@semantic-ui/core';
 import { $ } from '@semantic-ui/query';
 
 $('ui-button').on('click', () => {
-  $('.box').transition('toggle');
+  $('.box').transition('fade');
 });
