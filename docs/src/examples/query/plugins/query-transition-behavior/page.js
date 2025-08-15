a47700f0d738c3e9b4eafe7b9222270515bd021a
jlukic <jacklukic@gmail.com>
2025-08-15 17:36:48 -0400

    Docs: work on transition example

--- docs/src/examples/query/plugins/query-transition-behavior/page.js
+++ docs/src/examples/query/plugins/query-transition-behavior/page.js
@@ -2,6 +2,21 @@ import { Transition } from '@semantic-ui/core';
 import { $ } from '@semantic-ui/query';
 
 $('ui-button').on('click', () => {
-  const animation = $('select').val();
-  $('.box').transition(animation);
+  const animation = $('.animation').val();
+  const direction = $('.direction').val();
+  const duration = $('.duration').val() || 'auto';
+  const interval = $('.interval').val();
+
+  console.log({
+    animation,
+    direction,
+    duration,
+    interval,
+  });
+  $('.box').transition({
+    animation,
+    direction,
+    duration,
+    interval,
+  });
 });
