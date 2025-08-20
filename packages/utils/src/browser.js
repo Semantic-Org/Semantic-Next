1527eb2d528273620af6536a08afdbfe1d5f459f
jlukic <jacklukic@gmail.com>
2025-08-20 11:15:31 -0400

    Types: Fix missing type exports from utils

--- packages/utils/src/browser.js
+++ packages/utils/src/browser.js
@@ -51,7 +51,7 @@ export const getKeyFromEvent = (event) => {
 
 /* Wrapper for requestIdleCallback which does not have safari support */
 export const idleCallback = (callback) => {
-  if (isClient && window.requestIdleCallback) {
+  if (isClient && window.requestIdleCallback !== undefined) {
     requestIdleCallback(callback);
   }
   else {
