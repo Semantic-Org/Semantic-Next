e740ecf32e2cbf2c290d779a8b81dfc1b87b7035
jlukic <jacklukic@gmail.com>
2025-08-26 11:24:53 -0400

    Docs: finalize anchor example

--- docs/src/examples/query/plugins/query-anchor-behavior/page.js
+++ docs/src/examples/query/plugins/query-anchor-behavior/page.js
@@ -1,18 +1,19 @@
 import { Anchor, Transition } from '@semantic-ui/core';
 import { $ } from '@semantic-ui/query';
 
+// initialize popup inside page
 $('.outer .popup').anchor({
   to: '.outer .box',
-  offset: 0,
   position: 'top right',
 });
 
+// initialize popup inside scroll element
 $('.scroll .popup').anchor({
   to: '.scroll .box',
-  offset: 0,
   position: 'top right',
 });
 
+// show popup when a box is hovered
 $('.box')
   .on('mouseenter', function() {
     $('.popup').transition('scale top in');
