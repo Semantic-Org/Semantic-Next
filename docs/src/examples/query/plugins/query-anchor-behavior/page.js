773f2f74824a33e76566fe7b3dc66bc264ba29bd
jlukic <jacklukic@gmail.com>
2025-08-26 10:52:30 -0400

    Docs: add example of contain to scroll

--- docs/src/examples/query/plugins/query-anchor-behavior/page.js
+++ docs/src/examples/query/plugins/query-anchor-behavior/page.js
@@ -1,8 +1,14 @@
 import { Anchor, Transition } from '@semantic-ui/core';
 import { $ } from '@semantic-ui/query';
 
-$('.popup').anchor({
-  to: '.box',
+$('.outer .popup').anchor({
+  to: '.outer .box',
+  offset: 0,
+  position: 'top right',
+});
+
+$('.scroll .popup').anchor({
+  to: '.scroll .box',
   offset: 0,
   position: 'top right',
 });
