f9e418dd3166caa17c73284325ba3abf64b99b6c
jlukic <jacklukic@gmail.com>
2025-09-19 15:41:52 -0400

    Docs: improve offsetParent ex

--- docs/src/examples/query/dom-traversal/query-offsetparent/page.js
+++ docs/src/examples/query/dom-traversal/query-offsetparent/page.js
@@ -11,15 +11,14 @@ const log = (text) => $log.text(text);
 log(`Target offsetTop: ${$target.prop('offsetTop')}px`);
 
 // Toggle transform on box
-$('.toggle-transform').on('click', () => {
-  const currentTransform = $box.css('transform');
-  if (currentTransform === 'none') {
-    $box.css('transform', 'scale(1)');
-    log('Box transform set to: scale(1) - Now box is the offset parent!');
+$('.toggle.position').on('click', () => {
+  if ($target.computedStyle('position') === 'static') {
+    $target.css('position', 'fixed');
+    log('Target set to position: fixed');
   }
   else {
-    $box.css('transform', 'none');
-    log('Box transform set to: none');
+    $target.css('position', 'static');
+    log('Target set to position: static');
   }
 
   // Show how offsetTop is now relative to different element
@@ -29,8 +28,8 @@ $('.toggle-transform').on('click', () => {
 });
 
 // Toggle filter on box
-$('.toggle-filter').on('click', () => {
-  const currentFilter = $box.css('filter');
+$('.toggle.filter').on('click', () => {
+  const currentFilter = $box.computedStyle('filter');
   if (currentFilter === 'none') {
     $box.css('filter', 'brightness(1)');
     log('Box filter set to: brightness(1) - Now box is the offset parent!');
