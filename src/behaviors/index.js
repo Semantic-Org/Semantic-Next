510086f1aba8aacff8bc0d0fd05949494f52a059
Jack <jacklukic@gmail.com>
2026-01-14 18:40:47 -0500

    Feat: write first version of tooltip

--- src/behaviors/index.js
+++ src/behaviors/index.js
@@ -1,3 +1,4 @@
 // Export all behaviors as side effects so they register on import
 export { Attach } from './attach/attach.js';
+export { Tooltip } from './tooltip/tooltip.js';
 export { Transition } from './transition/transition.js';
