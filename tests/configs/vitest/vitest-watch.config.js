71adf54744262dcce112889cd3b3054cb5fe06a9
jlukic <jacklukic@gmail.com>
2026-01-26 11:54:27 -0500

    Chore: Remove any vistigial files in tests

---
+++ tests/configs/vitest/vitest-watch.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vitest/config';
+import node from './projects/node.js';
+import jsdom from './projects/jsdom.js';
+import browser from './projects/browser.js';
+
+export default defineConfig({
+  test: {
+    watch: true,
+    onConsoleLog(log) {
+      if (log.includes('Lit is in dev mode.')) return false;
+    },
+    projects: [node, jsdom, browser],
+  },
+});
