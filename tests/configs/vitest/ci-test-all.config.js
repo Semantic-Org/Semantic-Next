01864f230ac2ae3af4f8b819fc60e1f5afb475ac
jlukic <jacklukic@gmail.com>
2026-01-26 12:21:53 -0500

    Testing: Test out github action reporter

--- tests/configs/vitest/ci-test-all.config.js
+++ tests/configs/vitest/ci-test-all.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    reporter: ['basic', 'junit'],
+    reporter: ['basic', 'junit', 'github-actions'],
     outputFile: './tests/results/test-results-all-junit.xml',
     onConsoleLog (log) {
       if (log.includes('Lit is in dev mode.')) return false;
