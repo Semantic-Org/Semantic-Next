3c73be46942dacf69b4a1780f8a5f6c5e3f17508
jlukic <jacklukic@gmail.com>
2026-01-26 12:26:35 -0500

    Chore: Add fake failing test

---
+++ packages/utils/test/failing.test.js
@@ -0,0 +1,7 @@
+import { describe, expect, it } from 'vitest';
+
+describe('Failing Test', () => {
+  it('should fail to verify github-actions reporter', () => {
+    expect(1 + 1).toBe(3);
+  });
+});
