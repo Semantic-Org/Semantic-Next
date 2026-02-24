453490aa826d711f3ef614d926840c607597a89a
Jack Lukic <jacklukic@gmail.com>
2026-02-24 12:43:02 -0500

    Docs: update array examples

--- docs/src/examples/utils/arrays/utils-firstmatch/index.js
+++ docs/src/examples/utils/arrays/utils-firstmatch/index.js
@@ -1,5 +1,9 @@
 import { firstMatch } from '@semantic-ui/utils';
 
 const arr = [1, 2, 3, 4, 5];
-const result = firstMatch(arr, (value) => value > 3);
-console.log(result);
+
+// With a callback
+console.log(firstMatch(arr, (value) => value > 3));
+
+// With a value
+console.log(firstMatch(arr, 3));
