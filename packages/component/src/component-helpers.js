36284ad94c3c2d3fdf06f402d2a1ad070b914c4c
jlukic <jacklukic@gmail.com>
2026-03-30 11:17:01 -0400

    Refactor: Extract shared helpers from WebComponentBase
    Move renderer-agnostic methods (getProperties, getPropertySettings,
    setDefaultSettings, getSettingsFromConfig, createSettingsProxy,
    getUIClasses, isDarkMode) to component-helpers.js. WebComponentBase
    delegates to the extracted functions. Zero behavior change — all 621
    tests pass (573 renderer + 48 component).

---
+++ packages/component/src/component-helpers.js
@@ -0,0 +1,291 @@
+import { $ } from '@semantic-ui/query';
+import { Signal } from '@semantic-ui/reactivity';
+import {
+  camelToKebab,
+  each,
+  firstMatch,
+  get,
+  inArray,
+  isClassInstance,
+  isEqual,
+  isServer,
+  isString,
+  kebabToCamel,
+  keys,
+  unique,
+} from '@semantic-ui/utils';
+
+/*******************************
+         Lit Properties
+*******************************/
+
+/*
+  Builds property configuration from component spec and default settings.
+  Used by both WebComponentBase (standard) and LitWebComponentBase (Lit compat).
+*/
+export function getProperties({ properties = {}, defaultSettings, componentSpec }) {
+  if (keys(properties).length) {
+    return properties;
+  }
+  if (componentSpec) {
+    properties.class = { type: String, noAccessor: true, alias: true };
+
+    // emphasis="primary" but also setting props
+    each(componentSpec.attributes, (attributeName) => {
+      const propertyType = componentSpec.propertyTypes[attributeName];
+      const propertyName = kebabToCamel(attributeName);
+      properties[propertyName] = getPropertySettings({
+        name: attributeName,
+        type: propertyType,
+      });
+    });
+
+    // these are values that can only be set on the DOM el as properties
+    // but do not have attributes like functions or classes
+    each(componentSpec.properties, (attributeName) => {
+      const propertyType = componentSpec.propertyTypes[attributeName];
+      const propertyName = kebabToCamel(attributeName);
+      properties[propertyName] = getPropertySettings({
+        name: attributeName,
+        type: propertyType,
+      });
+    });
+
+    // this handles syntax where allowed value is used as attribute
+    // <ui-button primary> -> emphasis="primary"
+    each(componentSpec.optionAttributes, (attributeValues, attributeName) => {
+      const propertyName = kebabToCamel(attributeName);
+
+      // in some cases the value is the same as the category i.e. positive="positive"
+      if (properties[propertyName]) {
+        return;
+      }
+
+      properties[propertyName] = { type: String, noAccessor: true, alias: true, attribute: attributeName };
+    });
+  }
+  if (defaultSettings) {
+    each(defaultSettings, (defaultValue, propertyName) => {
+      // this can either be a settings object or a default value
+      // i.e. { foo: 'baz' } // basic
+      // or { foo: { type: String, defaultValue: 'baz' } // expert
+      properties[propertyName] = (defaultValue?.type)
+        ? defaultSettings
+        : getPropertySettings({
+          name: propertyName,
+          type: defaultValue?.constructor,
+          propertyOnly: isClassInstance(defaultValue), // cant serialize custom classes
+        });
+    });
+  }
+
+  /* This handles the case of multiword settings like `useAccordion`
+     we support 2 syntax <ui-menu use-accordion> or <ui-menu useaccordion>'
+     the kebab attr serves as an alias with no accessor
+  */
+  each(properties, (propertySettings, propertyName) => {
+    const attributeName = camelToKebab(propertyName);
+    if (attributeName !== propertyName && !properties[attributeName] && properties[propertyName]) {
+      properties[attributeName] = {
+        ...properties[propertyName],
+        noAccessor: true,
+        alias: true,
+      };
+    }
+  });
+
+  // accessors can break certain special dom attrs
+  // DISABLED FOR NOW
+  const specialAttrs = ['value', 'checked'];
+  each(specialAttrs, (attr) => {
+    if (properties[attr]) {
+      // properties[attr].noAccessor = true;
+    }
+  });
+  return properties;
+}
+
+export function getPropertySettings({ name, type = String, propertyOnly = false } = {}) {
+  // converts type = 'string' -> String
+  // this is because component spec cannot serialize prototypes in JSON
+  if (isString(type)) {
+    const types = {
+      string: String,
+      number: Number,
+      boolean: Boolean,
+      object: Object,
+      array: Array,
+      function: Function,
+    };
+    type = types[type] || String;
+  }
+
+  let property = {
+    type,
+    attribute: true,
+    hasChanged: (a, b) => {
+      return !isEqual(a, b);
+    },
+  };
+  // functions cannot be serialized
+  if (propertyOnly || type == Function) {
+    property.attribute = false;
+    property.hasChanged = (newVal, oldVal) => {
+      return true;
+    };
+  }
+  else if (type == Boolean) {
+    property.converter = {
+      fromAttribute: (value, type) => {
+        if (inArray(value, ['false', '0', 'null', 'undefined'])) {
+          return false;
+        }
+        if (inArray(value, ['', true, 'true'])) {
+          return true;
+        }
+        return Boolean(value);
+      },
+      toAttribute: (value, type) => {
+        return String(value);
+      },
+    };
+  }
+  return property;
+}
+
+/*******************************
+    Settings / Template Data
+*******************************/
+
+/*
+  Sets default settings for a component instance.
+  Call as: setDefaultSettings(el, { defaultSettings, componentSpec })
+*/
+export function setDefaultSettings(el, { defaultSettings = {}, componentSpec }) {
+  el.defaultSettings = { ...defaultSettings };
+  each(el.defaultSettings, (setting, name) => {
+    if (setting?.default !== undefined) {
+      el.defaultSettings[name] = setting.default;
+    }
+  });
+  if (componentSpec?.defaultValues) {
+    el.defaultSettings = {
+      ...componentSpec.defaultValues,
+      ...el.defaultSettings,
+    };
+  }
+}
+
+/*
+  Returns a list of settings which may include both attributes and properties
+  as specified in the spec for the component. Extends from default settings.
+  Call as: getSettingsFromConfig(el, { componentSpec, properties })
+*/
+export function getSettingsFromConfig(el, { componentSpec, properties }) {
+  let settings = {};
+  each(properties, (propSettings, propertyName) => {
+    if (propSettings.alias === true) {
+      return;
+    }
+    const elementProp = el[propertyName];
+    const setting = elementProp
+      ?? el.defaultSettings[propertyName]
+      ?? (componentSpec?.defaultSettings || {})[propertyName];
+    if (setting !== undefined) {
+      settings[propertyName] = setting;
+    }
+    if (componentSpec && settings[elementProp] !== undefined) {
+      settings[propertyName] = true;
+    }
+  });
+  return settings;
+}
+
+/*
+  Creates a proxy object which returns the current setting.
+  We need this over a getter/setter because settings are
+  destructured in function arguments which locks their value in time.
+  Call as: createSettingsProxy(el, { componentSpec, properties })
+*/
+export function createSettingsProxy(el, { componentSpec, properties }) {
+  el.settingsVars = new Map();
+  return new Proxy({}, {
+    get: (target, property) => {
+      const settings = el.getSettings({
+        componentSpec,
+        properties,
+      });
+      const setting = get(settings, property);
+      let signal = el.settingsVars.get(property);
+      if (!signal) {
+        signal = new Signal(setting);
+        el.settingsVars.set(property, signal);
+      }
+      signal.set(setting);
+      signal.get();
+      return setting;
+    },
+    set: (target, property, value, receiver) => {
+      el.setSetting(property, value);
+      let signal = el.settingsVars.get(property);
+      if (signal) {
+        signal.set(value);
+      }
+      else {
+        signal = new Signal(value);
+        el.settingsVars.set(property, signal);
+      }
+      return true;
+    },
+  });
+}
+
+/*
+  Returns styling classes for the template data context based on
+  component spec attributes.
+  Call as: getUIClasses(el, { componentSpec, properties })
+*/
+export function getUIClasses(el, { componentSpec, properties }) {
+  if (!componentSpec) {
+    return;
+  }
+  const classes = [];
+  each(componentSpec.attributes, (attribute) => {
+    const property = kebabToCamel(attribute);
+    const value = el[property] || el[attribute];
+    if (value) {
+      const allowedValues = componentSpec.allowedValues?.[attribute];
+      const propertyType = componentSpec.propertyTypes?.[attribute];
+      if (propertyType == 'boolean') {
+        classes.push(attribute);
+      }
+      else if (value == attribute && allowedValues && inArray(value, allowedValues)) {
+        classes.push(attribute);
+      }
+      else if (allowedValues && inArray(value, allowedValues)) {
+        const compoundForms = [`${value}-${attribute}`, `${attribute}-${value}`];
+        const compoundClass = firstMatch(compoundForms, (form) => componentSpec.optionAttributes?.[form]);
+        classes.push(compoundClass || value);
+      }
+      else if (value === true && inArray(property, allowedValues)) {
+        classes.push(property);
+      }
+
+      if ((componentSpec.attributeClasses || []).includes(attribute)) {
+        classes.push(attribute);
+      }
+    }
+  });
+
+  let classString = unique(classes).join(' ');
+  if (classString) {
+    classString += ' ';
+  }
+  return classString;
+}
+
+export function isDarkMode(el) {
+  return (isServer)
+    ? undefined
+    : $('html').hasClass('dark') || $(el).cssVar('dark-mode') == 'true';
+}
--- packages/component/src/web-component.js
+++ packages/component/src/web-component.js
@@ -1,25 +1,24 @@
 import { $ } from '@semantic-ui/query';
-import { Signal } from '@semantic-ui/reactivity';
 import {
-  camelToKebab,
   each,
-  firstMatch,
-  get,
-  inArray,
-  isClassInstance,
-  isEqual,
   isFunction,
-  isServer,
-  isString,
-  kebabToCamel,
-  keys,
-  unique,
 } from '@semantic-ui/utils';
 import { LitElement } from 'lit';
 
+import {
+  createSettingsProxy,
+  getProperties,
+  getPropertySettings,
+  getSettingsFromConfig,
+  getUIClasses,
+  isDarkMode,
+  setDefaultSettings,
+} from './component-helpers.js';
+
 /*
-  This extends the base Lit element class to include
-  additional functionality useful for Semantic UI
+  Lit-backed web component base class.
+  Used when renderingEngine is 'lit' (the default for backwards compatibility).
+  Shared logic lives in component-helpers.js.
 */
 
 class WebComponentBase extends LitElement {
@@ -59,295 +58,36 @@ class WebComponentBase extends LitElement {
            Lit Properties
   *******************************/
 
-  static getProperties({ properties = {}, defaultSettings, componentSpec }) {
-    if (keys(properties).length) {
-      return properties;
-    }
-    if (componentSpec) {
-      properties.class = { type: String, noAccessor: true, alias: true };
-
-      // emphasis="primary" but also setting props
-      each(componentSpec.attributes, (attributeName) => {
-        const propertyType = componentSpec.propertyTypes[attributeName];
-        const propertyName = kebabToCamel(attributeName);
-        properties[propertyName] = WebComponentBase.getPropertySettings({
-          name: attributeName,
-          type: propertyType,
-        });
-      });
-
-      // these are values that can only be set on the DOM el as properties
-      // but do not have attributes like functions or classes
-      each(componentSpec.properties, (attributeName) => {
-        const propertyType = componentSpec.propertyTypes[attributeName];
-        const propertyName = kebabToCamel(attributeName);
-        properties[propertyName] = WebComponentBase.getPropertySettings({
-          name: attributeName,
-          type: propertyType,
-        });
-      });
-
-      // this handles syntax where allowed value is used as attribute
-      // <ui-button primary> -> emphasis="primary"
-      each(componentSpec.optionAttributes, (attributeValues, attributeName) => {
-
-        const propertyName = kebabToCamel(attributeName);
-
-        // in some cases the value is the same as the category i.e. positive="positive"
-        if(properties[propertyName]) {
-          return;
-        }
-
-        properties[propertyName] = { type: String, noAccessor: true, alias: true, attribute: attributeName };
-      });
-    }
-    if (defaultSettings) {
-      each(defaultSettings, (defaultValue, propertyName) => {
-        // this can either be a settings object or a default value
-        // i.e. { foo: 'baz' } // basic
-        // or { foo: { type: String, defaultValue: 'baz' } // expert
-        properties[propertyName] = (defaultValue?.type)
-          ? defaultSettings
-          : WebComponentBase.getPropertySettings({
-            name: propertyName,
-            type: defaultValue?.constructor,
-            propertyOnly: isClassInstance(defaultValue), // cant serialize custom classes
-          });
-      });
-    }
-
-    /* This handles the case of multiword settings like `useAccordion`
-       we support 2 syntax <ui-menu use-accordion> or <ui-menu useaccordion>'
-       the kebab attr serves as an alias with no accessor
-    */
-    each(properties, (propertySettings, propertyName) => {
-      const attributeName = camelToKebab(propertyName);
-      if (attributeName !== propertyName && !properties[attributeName] && properties[propertyName]) {
-        properties[attributeName] = {
-          ...properties[propertyName],
-          noAccessor: true,
-          alias: true,
-        };
-      }
-    });
-
-    // accessors can break certain special dom attrs
-    // DISABLED FOR NOW
-    const specialAttrs = ['value', 'checked'];
-    each(specialAttrs, (attr) => {
-      if (properties[attr]) {
-        // properties[attr].noAccessor = true;
-      }
-    });
-    //console.log(properties);
-    return properties;
+  static getProperties(options) {
+    return getProperties(options);
   }
 
-  static getPropertySettings({ name, type = String, propertyOnly = false } = {}) {
-    // converts type = 'string' -> String
-    // this is because compont spec cannot serialize prototypes in JSON
-    if (isString(type)) {
-      const types = {
-        string: String,
-        number: Number,
-        boolean: Boolean,
-        object: Object,
-        array: Array,
-        function: Function,
-      };
-      type = types[type] || String;
-    }
-
-    let property = {
-      type,
-      attribute: true,
-      hasChanged: (a, b) => {
-        return !isEqual(a, b);
-      },
-    };
-    // functions cannot be serialized
-    if (propertyOnly || type == Function) {
-      property.attribute = false;
-      property.hasChanged = (newVal, oldVal) => {
-        return true;
-      };
-    }
-    else if (type == Boolean) {
-      property.converter = {
-        fromAttribute: (value, type) => {
-          if (inArray(value, ['false', '0', 'null', 'undefined'])) {
-            return false;
-          }
-          if (inArray(value, ['', true, 'true'])) {
-            return true;
-          }
-          return Boolean(value);
-        },
-        toAttribute: (value, type) => {
-          return String(value);
-        },
-      };
-    }
-    return property;
+  static getPropertySettings(options) {
+    return getPropertySettings(options);
   }
 
   /*******************************
       Settings / Template Data
   *******************************/
 
-  /*
-    This sets default settings for a component
-  */
-  setDefaultSettings({ defaultSettings = {}, componentSpec }) {
-    this.defaultSettings = defaultSettings;
-    each(defaultSettings, (setting, name) => {
-      if (setting?.default !== undefined) {
-        // (expert) this is a settings object
-        this.defaultSettings[name] = setting.default;
-      }
-      else {
-        // (simple) this is just a default value
-        this.defaultSettings[name] = setting;
-      }
-    });
-    // read default values from component spec if available
-    if (componentSpec?.defaultValues) {
-      this.defaultSettings = {
-        ...componentSpec.defaultValues,
-        ...this.defaultSettings,
-      };
-    }
+  setDefaultSettings(options) {
+    setDefaultSettings(this, options);
   }
 
-  /*
-    This returns a list of settings which may include both attributes and properties
-    as specified in the spec for the component. It will extend them from default settings
-  */
-  getSettingsFromConfig({ componentSpec, properties }) {
-    let settings = {};
-    each(properties, (propSettings, propertyName) => {
-      if (propSettings.alias === true) {
-        return;
-      }
-      const elementProp = this[propertyName];
-      const setting = elementProp // check element setting
-        ?? this.defaultSettings[propertyName] // check default setting on this component
-        ?? (componentSpec?.defaultSettings || {})[propertyName] // check default setting on component spec
-      ;
-      // only pass through setting if it is defined
-      if (setting !== undefined) {
-        settings[propertyName] = setting;
-      }
-      // boolean attribute case
-      if (componentSpec && settings[elementProp] !== undefined) {
-        settings[propertyName] = true;
-      }
-    });
-    return settings;
+  getSettingsFromConfig(options) {
+    return getSettingsFromConfig(this, options);
   }
 
-  /* Create a proxy object which returns the current setting
-     we need this over a getter/setter because settings are
-     destructured in function arguments which locks their value in time
-     i.e. onCreated({settings}) { }
-  */
-  createSettingsProxy({ componentSpec, properties }) {
-    let component = this;
-    /*
-      To make settings reactive in Reactions
-      we need to map them to signals
-    */
-    component.settingsVars = new Map();
-    return new Proxy({}, {
-      get: (target, property) => {
-        const settings = component.getSettings({
-          componentSpec,
-          properties,
-        });
-        const setting = get(settings, property);
-        let signal = component.settingsVars.get(property);
-        if (!signal) {
-          signal = new Signal(setting);
-          component.settingsVars.set(property, signal);
-        }
-        signal.set(setting);
-        signal.get();
-        return setting;
-      },
-      set: (target, property, value, receiver) => {
-        component.setSetting(property, value);
-        let signal = component.settingsVars.get(property);
-        if (signal) {
-          signal.set(value);
-        }
-        else {
-          signal = new Signal(value);
-          component.settingsVars.set(property, signal);
-        }
-        return true;
-      },
-    });
+  createSettingsProxy(options) {
+    return createSettingsProxy(this, options);
   }
 
-  /*
-    This returns a list of styling classes to pass through to
-    the template data context for the component based off
-    component attributes
-  */
-  getUIClasses({ componentSpec, properties }) {
-    // this is just a special feature of component specs and can be ignored otherwise
-    if (!componentSpec) {
-      return;
-    }
-    const classes = [];
-    // iterate through tracked attributes which can receive classes
-    each(componentSpec.attributes, (attribute) => {
-      const property = kebabToCamel(attribute);
-      const value = this[property] || this[attribute];
-      if (value) {
-        const allowedValues = componentSpec.allowedValues?.[attribute];
-        const propertyType = componentSpec.propertyTypes?.[attribute];
-        if (propertyType == 'boolean') {
-          // this is a variation like active=true
-          // it receives the class "active"
-          classes.push(attribute);
-        }
-        else if (value == attribute && allowedValues && inArray(value, allowedValues)) {
-          // disabled="disabled" use case operates like boolean
-          classes.push(attribute);
-        }
-        else if (allowedValues && inArray(value, allowedValues)) {
-          // this is a variation like emphasis="primary"
-          // check if value requires compound form for CSS class (e.g. "subtle" → "subtle-positive")
-          const compoundForms = [`${value}-${attribute}`, `${attribute}-${value}`];
-          const compoundClass = firstMatch(compoundForms, (form) => componentSpec.optionAttributes?.[form]);
-          classes.push(compoundClass || value);
-        }
-        else if (value === true && inArray(property, allowedValues)) {
-          // this is identity like positive="true"
-          classes.push(property);
-        }
-
-
-        // components can opt-in to including the attribute if it has a value set
-        // for instance "icon" if it has an icon set
-        if ((componentSpec.attributeClasses || []).includes(attribute)) {
-          classes.push(attribute);
-        }
-      }
-    });
-
-    let classString = unique(classes).join(' ');
-    if (classString) {
-      classString += ' ';
-    }
-    return classString;
+  getUIClasses(options) {
+    return getUIClasses(this, options);
   }
 
   isDarkMode() {
-    return (isServer)
-      ? undefined
-      : $('html').hasClass('dark') || $(this).cssVar('dark-mode') == 'true';
+    return isDarkMode(this);
   }
 
   /*******************************
