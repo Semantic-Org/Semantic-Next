a97abcdcb703bc26f67ead8b45d60ef79f139cfa
Jack Lukic <jacklukic@gmail.com>
2026-04-04 23:01:06 -0400

    Feat: Add generic inference for settings, state, and self in defineComponent

--- packages/component/types/index.d.ts
+++ packages/component/types/index.d.ts
@@ -1,7 +1,7 @@
 export { registerHelper, registerHelpers } from '@semantic-ui/templating';
 export { getJSON, getText } from '@semantic-ui/utils';
 
-export { defineComponent, DefineComponentOptions, UIWebComponent } from './define-component';
+export { defineComponent, UIWebComponent } from './define-component';
 export { WebComponentBase } from './web-component';
 
 export { adoptStylesheet, extractCSS, scopeStyles } from '@semantic-ui/utils';
