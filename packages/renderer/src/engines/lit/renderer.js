c0c189f220f6d2e88e4f284c8ee189dc2871c485
jlukic <jacklukic@gmail.com>
2026-04-08 18:19:13 -0400

    Perf: Fix dataVersion to use more performant signal

--- packages/renderer/src/engines/lit/renderer.js
+++ packages/renderer/src/engines/lit/renderer.js
@@ -14,6 +14,7 @@ import {
   isString,
   keys,
   mapObject,
+  noop,
   wrapFunction,
 } from '@semantic-ui/utils';
 
@@ -57,7 +58,7 @@ export class LitRenderer {
     this.inheritsData = inheritsData; // for subtrees lets us know if this needs to have data updates downstream
     this.protectedKeys = protectedKeys; // keys scoped to this subtree (loop vars, async results) that parent updates cannot overwrite
     this.id = LitRenderer.getID({ ast, data, isSVG });
-    this.dataVersion = new Signal(0);
+    this.dataVersion = new Signal(0, { allowClone: false, isEqual: noop });
 
     // Delegate expression evaluation
     this.evaluator = new ExpressionEvaluator({
