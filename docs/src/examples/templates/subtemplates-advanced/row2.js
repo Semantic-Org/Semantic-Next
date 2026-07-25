e805e6ad09231e10b0c9c7c262a03420512cf8db
Jack Lukic <jacklukic@gmail.com>
2026-07-24 22:32:25 -0400

    Docs: Retire duplicate examples and merge aliases

--- docs/src/examples/templates/subtemplates-advanced/row2.js
+++
@@ -1,9 +0,0 @@
-import { defineComponent, getText } from '@semantic-ui/component';
-
-const css = await getText('./row2.css');
-const template = await getText('./row2.html');
-
-export const Row2 = defineComponent({
-  template,
-  css,
-});
