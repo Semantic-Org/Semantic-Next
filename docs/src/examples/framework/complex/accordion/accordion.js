983b7a570c39efdb004eb711156f4c5b0adc8e41
jlukic <jacklukic@gmail.com>
2026-07-27 11:56:36 -0400

    Docs: Annotate accordion setting and use property binding for checked

--- docs/src/examples/framework/complex/accordion/accordion.js
+++ docs/src/examples/framework/complex/accordion/accordion.js
@@ -5,7 +5,7 @@ const css = await getText('./component.css');
 const template = await getText('./component.html');
 
 const defaultSettings = {
-  sections: [],
+  sections: [], // sections that can be opened
 };
 
 const defaultState = {
