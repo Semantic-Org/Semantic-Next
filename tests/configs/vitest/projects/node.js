80b0901ba4d5a8e002aaf0b92bc8638d89551563
jlukic <jacklukic@gmail.com>
2026-07-30 00:07:24 -0400

    Test: Exclude agent worktrees from vitest collection

--- tests/configs/vitest/projects/node.js
+++ tests/configs/vitest/projects/node.js
@@ -1,10 +1,12 @@
+import { exclude } from './exclude.js';
+
 export default {
   test: {
     include: [
       '**/test/unit/**/*.test.{ts,js}',
       '**/test/*.test.{ts,js}'
     ],
-    exclude: ['**/node_modules/**', 'docs/**'],
+    exclude,
     name: 'node',
     environment: 'node',
     setupFiles: ['tests/setup/node-setup.js'],
